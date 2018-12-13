304a9f7d4c88751bbe90c26fd10f0040d42427db
Avazkhon <kamalxanovavazxon@gmail.com>
2018-12-13 17:46:15 +0700

    react

---
+++ src/content/content.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Content () {
+	return <div className="content">content</div>
+}
+
+export default Content
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = 'Update My Minimal React Webpack Babel Setup';
+import Footer from './footer/footer.js'
+import Content from './content/content.js'
+
+function Conteiner () {
+	return (
+		<div className='conteiner'>
+			{Footer()}
+			{Content()}
+		</div>
+	)
+};
 
 ReactDOM.render(
-  <div>{title}</div>,
+  Conteiner(),
   document.getElementById('app')
 );
 
